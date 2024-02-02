eb489dd49834b2735fa10f96beb1606291ff47d9
gord chung <gord@live.ca>
2024-02-02 14:54:07 -0500

    add sqlx connection

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ mod views;
 mod tests;
 
 use std::sync::Arc;
+use std::time::Duration;
 
-use axum::{routing::get, Router};
+use axum::{extract::State, response::IntoResponse, routing::get, Router};
 use minijinja::{path_loader, Environment};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use tower_http::{services::ServeDir, trace::TraceLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -18,6 +20,7 @@ use crate::settings::Settings;
 use crate::views::pages;
 
 pub struct AppState {
+    pub db: Pool<Postgres>,
     pub jinja: Environment<'static>,
     pub settings: Settings,
 }
@@ -38,18 +41,26 @@ async fn main() {
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     tracing::info!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app()).await.unwrap();
+    axum::serve(listener, app().await).await.unwrap();
 }
 
-fn app() -> Router {
+async fn app() -> Router {
     // setup jinja
     let mut minijinja = Environment::new();
     minijinja.set_loader(path_loader("src/templates"));
+
     let settings = Settings::new().unwrap();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&settings.database_uri)
+        .await
+        .expect("Error creating db pool");
 
     // application routes
     Router::new()
-        .route("/health", get(|| async { "¯\\_(ツ)_/¯" }))
+        .route("/health", get(healthcheck))
         .route("/api/v1/data/random", get(v1::routes::random_data))
         .route("/api/v1/data/sales", get(v1::routes::sales_data))
         .route("/api/v1/market/prices", get(v1::routes::price_data))
@@ -62,8 +73,14 @@ fn app() -> Router {
         .route("/yahoo", get(pages::yahoo))
         .nest_service("/static/js", ServeDir::new("src/static/js"))
         .with_state(Arc::new(AppState {
+            db: pool,
             jinja: minijinja,
             settings,
         }))
         .layer(TraceLayer::new_for_http())
+}
+
+async fn healthcheck(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    let _ = sqlx::query("select 1").fetch_one(&state.db).await.unwrap();
+    "¯\\_(ツ)_/¯"
 }
